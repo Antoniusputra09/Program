f1cb6e67308ef75692c7bd918e651d265beac185
Antoniusputra09 <antoniusputra7@gmail.com>
2018-12-28 14:16:15 +0700

    aa

--- app/src/main/java/com/example/asus/program/haldaftar.java
+++ app/src/main/java/com/example/asus/program/haldaftar.java
@@ -26,7 +26,8 @@ import java.util.HashMap;
 public class haldaftar extends AppCompatActivity {
 
     String email, pass1, pass2;
-    EditText edit1, edit2, edit3, edit4;
+    EditText edit1, edit2, edit3, edit4, kelas, ttl, tempat, Nis, alamat,
+            nohp, namaayah, noayah, namaibu, noibu, namawali, nowali, catatan;
     Button btn;
     FirebaseAuth auth;
     DatabaseReference databaseReference;
@@ -36,11 +37,24 @@ public class haldaftar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_haldaftar);
-
         edit1= (EditText)findViewById(R.id.daftaremail);
         edit2= (EditText) findViewById(R.id.daftarpass);
         edit3= (EditText) findViewById(R.id.ulangpass);
         edit4 = (EditText) findViewById(R.id.daftarusername);
+        kelas = (EditText) findViewById(R.id.kelas);
+        ttl = (EditText) findViewById(R.id.ttl);
+        tempat = (EditText) findViewById(R.id.tempat);
+        Nis = (EditText) findViewById(R.id.Nis);
+        alamat = (EditText) findViewById(R.id.alamat);
+        nohp = (EditText) findViewById(R.id.nohp);
+        namaayah = (EditText) findViewById(R.id.namaayah);
+        noayah = (EditText) findViewById(R.id.noayah);
+        namaibu = (EditText) findViewById(R.id.namaibu);
+        noibu = (EditText) findViewById(R.id.noibu);
+        namawali = (EditText) findViewById(R.id.namawali);
+        nowali = (EditText) findViewById(R.id.nowali);
+        catatan = (EditText) findViewById(R.id.catatan);
+
         btn= (Button) findViewById(R.id.tomboldaftar);
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Harap Tunggu");
@@ -55,6 +69,19 @@ public class haldaftar extends AppCompatActivity {
                 String txt_username = edit4.getText().toString();
                 String txt_email = edit1.getText().toString();
                 String txt_password = edit3.getText().toString();
+                String txt_kelas = kelas.getText().toString();
+                String txt_ttl = ttl.getText().toString();
+                String txt_tempat = tempat.getText().toString();
+                String txt_Nis = Nis.getText().toString();
+                String txt_alamat = alamat.getText().toString();
+                String txt_nohp = nohp.getText().toString();
+                String txt_namaayah = namaayah.getText().toString();
+                String txt_noayah = noayah.getText().toString();
+                String txt_namaibu = namaibu.getText().toString();
+                String txt_noibu = noibu.getText().toString();
+                String txt_namawali = namawali.getText().toString();
+                String txt_nowali = nowali.getText().toString();
+                String txt_catatan = catatan.getText().toString();
 
                 if (TextUtils.isEmpty(txt_username) || TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password) ){
                     progressDialog.dismiss();
@@ -65,7 +92,9 @@ public class haldaftar extends AppCompatActivity {
                     progressDialog.dismiss();
                 }
                 else{
-                    daftar(txt_username, txt_email, txt_password);
+                    daftar(txt_username, txt_email, txt_password, txt_kelas, txt_ttl, txt_tempat, txt_Nis, txt_alamat, txt_nohp,
+                            txt_namaayah, txt_noayah, txt_namaibu, txt_noibu, txt_namawali, txt_nowali, txt_catatan);
+
                     progressDialog.dismiss();
                     startActivity(new Intent(haldaftar.this,MainActivity.class));
                 }
@@ -75,7 +104,9 @@ public class haldaftar extends AppCompatActivity {
         //daftar();
     }
 
-    private void daftar(final String username, String email, String password){
+    private void daftar(final String username, final String email, String password, final String kelas, final String ttl, final String tempat, final String nis,
+                        final String alamat, final String nohp, final String namayah, final String noayah, final String namaibu, final String noibu, final String namawali,
+                        final String nowali, final String catatan){
 
         auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -90,7 +121,21 @@ public class haldaftar extends AppCompatActivity {
 
                             HashMap<String, String> hashMap = new HashMap<>();
                             hashMap.put("id", userid);
+                            hashMap.put("email", email);
                             hashMap.put("username", username);
+                            hashMap.put("kelas", kelas);
+                            hashMap.put("ttl", ttl);
+                            hashMap.put("tempat", tempat);
+                            hashMap.put("nis", nis);
+                            hashMap.put("alamat", alamat);
+                            hashMap.put("nohp", nohp);
+                            hashMap.put("namaayah", namayah);
+                            hashMap.put("noayah", noayah);
+                            hashMap.put("namaibu", namaibu);
+                            hashMap.put("noibu", noibu);
+                            hashMap.put("namawali", namawali);
+                            hashMap.put("nowali", nowali);
+                            hashMap.put("catatan", catatan);
                             hashMap.put("imageUrl", "Default");
 
                             databaseReference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
